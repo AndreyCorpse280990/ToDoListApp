42fe096496242b436222e14166f8406fccdd48ec
AndreyCorpse28090 <280990corpse@gmail.com>
2024-04-05 21:26:21 +0300

    Добавьте файлы проекта.

---
+++ ToDoListApp/MainForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ToDoListApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.descritionTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.priorityComboBox = new System.Windows.Forms.ComboBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.toDoListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // descritionTextBox
+            // 
+            this.descritionTextBox.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.descritionTextBox.Location = new System.Drawing.Point(160, 4);
+            this.descritionTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.descritionTextBox.Name = "descritionTextBox";
+            this.descritionTextBox.Size = new System.Drawing.Size(282, 39);
+            this.descritionTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(2, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Описание:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(452, 6);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Приоритет:";
+            // 
+            // priorityComboBox
+            // 
+            this.priorityComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.priorityComboBox.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.priorityComboBox.FormattingEnabled = true;
+            this.priorityComboBox.Items.AddRange(new object[] {
+            "Низкий",
+            "Средний",
+            "Высокий"});
+            this.priorityComboBox.Location = new System.Drawing.Point(626, 6);
+            this.priorityComboBox.Margin = new System.Windows.Forms.Padding(2);
+            this.priorityComboBox.Name = "priorityComboBox";
+            this.priorityComboBox.Size = new System.Drawing.Size(282, 40);
+            this.priorityComboBox.TabIndex = 3;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addButton.Location = new System.Drawing.Point(9, 62);
+            this.addButton.Margin = new System.Windows.Forms.Padding(2);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(274, 44);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click_1);
+            // 
+            // editButton
+            // 
+            this.editButton.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.editButton.Location = new System.Drawing.Point(288, 62);
+            this.editButton.Margin = new System.Windows.Forms.Padding(2);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(274, 44);
+            this.editButton.TabIndex = 5;
+            this.editButton.Text = "Изменить";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteButton.Location = new System.Drawing.Point(567, 62);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(2);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(340, 44);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // toDoListBox
+            // 
+            this.toDoListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.toDoListBox.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toDoListBox.FormattingEnabled = true;
+            this.toDoListBox.ItemHeight = 32;
+            this.toDoListBox.Location = new System.Drawing.Point(9, 124);
+            this.toDoListBox.Margin = new System.Windows.Forms.Padding(2);
+            this.toDoListBox.Name = "toDoListBox";
+            this.toDoListBox.Size = new System.Drawing.Size(892, 516);
+            this.toDoListBox.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 700);
+            this.Controls.Add(this.toDoListBox);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.priorityComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.descritionTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(942, 739);
+            this.Name = "MainForm";
+            this.Text = "Список дел";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox descritionTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox priorityComboBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.ListBox toDoListBox;
+    }
+}
+
---
+++ ToDoListApp/MainForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoListApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //
+            priorityComboBox.SelectedIndex = 0;
+        }
+
+        private void addButton_Click_1(object sender, EventArgs e)
+        {
+            string description = descritionTextBox.Text;
+            string priority = priorityComboBox.SelectedItem.ToString();
+            int position = toDoListBox.Items.Count + 1;
+
+            if (description != "")
+            {
+                string newItem = $"{position}. {description} - {priority}";
+                toDoListBox.Items.Add(newItem);
+                descritionTextBox.Clear();
+                priorityComboBox.SelectedIndex = 0;
+            } else {
+                MessageBox.Show
+                    (" введите описание  для нового дела.");
+            }         
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if( toDoListBox.SelectedIndex != -1 )
+            {
+
+                int removeIndex = toDoListBox.SelectedIndex;
+                toDoListBox.Items.RemoveAt(toDoListBox.SelectedIndex);
+
+                for(int i = removeIndex; i < toDoListBox.Items.Count; i++)
+                { 
+                    string itemText = toDoListBox.Items[i].ToString();
+
+                    toDoListBox.Items[i] = $"{ i + 1}. {itemText.Substring(itemText.IndexOf('.') + 2)}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите дело для удаления");
+            }
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            if (toDoListBox.SelectedIndex != -1)
+            {
+                string description = descritionTextBox.Text;
+                string priority = priorityComboBox.SelectedItem?.ToString();
+                int selectedIndex = toDoListBox.SelectedIndex;
+
+                if (description != "")
+                {
+                    string editItem = $"{selectedIndex + 1}. {description} - {priority}";
+                    toDoListBox.Items[selectedIndex] = editItem;
+                    descritionTextBox.Clear();
+                    priorityComboBox.SelectedIndex = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Введите описание для редактируемого дела.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите дело для редактирования.");
+            }
+        }
+
+    }
+
+
+}
+
